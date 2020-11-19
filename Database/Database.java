e0cc4345d6414ba6f80b35387c19b9d668df126c
Walim Abdul Somad <Pusdatin@walim.local>
2020-11-19 17:55:20 +0700

    after rain

--- Database/Database.java
+++ Database/Database.java
@@ -8,9 +8,12 @@ public class Database {
 
     public static Connection getKoneksi() {
         try {
-            String url = "jdbc:mysql://103.129.220.10:3306/parkir?serverTimezone=Asia/Jakarta";
-            String user = "user123";
-            String password = "22v4mt@Z";
+            // String url = "jdbc:mysql://103.129.220.10:3306/parkir?serverTimezone=Asia/Jakarta";
+            // String user = "user123";
+            // String password = "22v4mt@Z";
+            String url = "jdbc:mysql://127.0.0.1:3306/parkir?serverTimezone=Asia/Jakarta";
+            String user = "root";
+            String password = "Rahas1a!";
             con = DriverManager.getConnection(url, user, password);
             if (con == null) {
                 System.out.println("Failed to make connection!");
---
+++ Database/parkir202011191153.sql
@@ -0,0 +1,40 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for macos10.13 (x86_64)
+--
+-- Host: 103.129.220.10    Database: parkir
+-- ------------------------------------------------------
+-- Server version	5.5.68-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8mb4 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `area`
+--
+
+DROP TABLE IF EXISTS `area`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `area` (
+  `IdArea` int(11) NOT NULL AUTO_INCREMENT,
+  `namaArea` varchar(255) NOT NULL,
+  PRIMARY KEY (`IdArea`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `area`
+--
+
+LOCK TABLES `area` WRITE;
+/*!40000 ALTER TABLE `area` DISABLE KEYS */;
+INSERT INTO `area` VALUES (1,'CoolParkingArea'),(2,'NaissParking');
+/*!40000 ALTER TABLE `area` ENABLE KEYS */;
+UNLOCK TABLES;
